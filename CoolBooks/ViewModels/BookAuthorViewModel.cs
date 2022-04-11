a29c4096ce672c36fddd31e3eb2531c4c4076784
centrifug <c3ntrifug@gmail.com>
2022-04-12 00:04:06 +0200

    fixat add book!

---
+++ CoolBooks/ViewModels/BookAuthorViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace CoolBooks.ViewModels
+{
+    public class BookAuthorViewModel
+    {
+        public int AuthorId { get; set; }
+        public string AuthorFirstName { get; set; }
+        public string AuthorLastName { get; set; }
+        public bool IsSelected { get; set; }
+    }
+}
