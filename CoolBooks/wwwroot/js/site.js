daeed7ee1665ef63f004b9181e5fec0c0901da77
centrifug <c3ntrifug@gmail.com>
2022-05-01 23:23:44 +0200

    lagt till sökrutor på author/genre i create-book

--- CoolBooks/wwwroot/js/site.js
+++ CoolBooks/wwwroot/js/site.js
@@ -3,3 +3,35 @@
 
 // Write your JavaScript code.
 
+
+// Sökfunktion för authors i create-book
+$(document).ready(function () {
+    $('#filterAuthor').keyup(function () {
+        var query = this.value;
+
+        $(".author").each(function () {
+
+            $(this).hide();
+            if ($(this).text().toLowerCase().indexOf(query.toLowerCase()) != -1) {
+                $(this).show();
+            }
+        });
+
+    });
+});
+
+// Sökfunktion för Genre i create-book
+$(document).ready(function () {
+    $('#filterGenre').keyup(function () {
+        var query = this.value;
+
+        $(".genre").each(function () {
+
+            $(this).hide();
+            if ($(this).text().toLowerCase().indexOf(query.toLowerCase()) != -1) {
+                $(this).show();
+            }
+        });
+
+    });
+});
